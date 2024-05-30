f82dff41c0233550c91e0d353a348b3e6b2aa50c
Ha Manh Do <v.hadm7@vinai.io>
2024-05-30 18:00:35 +0700

    Prepare NextJS front-end stuffs

---
+++ go-sqlite-nginx-redis-react-crud/go-api/initializer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() error {
+	var err error
+
+	DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	DB.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running Migrations")
+	err = DB.AutoMigrate(&Note{})
+	if err != nil {
+		return err
+	}
+
+	log.Println("🚀 Connected Successfully to the Database")
+	return nil
+}
